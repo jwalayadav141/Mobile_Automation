0c10a2efe11c1c47eb5b342cf07928bb78421dcf
Jwala Yadav <jwalayadav141@gmail.com>
2024-10-02 09:19:26 +0530

    Framework Creation

---
+++ MobileAutomationFramework/src/main/java/org/apache/maven/MobileAutomationFramework/App.java
@@ -0,0 +1,10 @@
+package org.apache.maven.MobileAutomationFramework;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
